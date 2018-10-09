9b2e283744f26dfc1b1d1952c86c057b69d633f4
shahidmh <shahidmh@users.noreply.github.com>
2018-10-09 11:51:10 +0530

    added environment variable

--- db.js
+++ db.js
@@ -3,11 +3,11 @@
 
 
 var mongoose = require('mongoose');
+var env = require('dotenv').load();
+COSMOSDB_CONNSTR="mongodb://portal2018:df4Xz3DN5NFDgJ6vsDMkS7yct4zUppTK0nrAlKqBRjhBndpWvLQcK7ulPlyJ2vEvpp1DuWgY8ljgHLThOGe3OA==@portal2018.documents.azure.com:10255/?ssl=true"
+COSMOSDB_DBNAME="portal2018"
 
-// COSMOSDB_CONNSTR={mongodb://portal2018:df4Xz3DN5NFDgJ6vsDMkS7yct4zUppTK0nrAlKqBRjhBndpWvLQcK7ulPlyJ2vEvpp1DuWgY8ljgHLThOGe3OA==@portal2018.documents.azure.com:10255/?ssl=true}
-// COSMOSDB_DBNAME={portal2018}
-
-mongoose.connect("mongodb://portal2018:df4Xz3DN5NFDgJ6vsDMkS7yct4zUppTK0nrAlKqBRjhBndpWvLQcK7ulPlyJ2vEvpp1DuWgY8ljgHLThOGe3OA%3D%3D@portal2018.documents.azure.com:10255/?ssl=true"); //Creates a new DB, if it doesn't already exist
+mongoose.connect(process.env.COSMOSDB_CONNSTR+process.env.COSMOSDB_DBNAME+"?ssl=true&replicaSet=globaldb"); //Creates a new DB, if it doesn't already exist
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
