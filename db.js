d0d47587c85fe4b0763494e719c893d9677c161c
shahidmh <shahidmh@users.noreply.github.com>
2018-10-09 12:33:42 +0530

    undo

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 // COSMOSDB_CONNSTR={mongodb://portal2018:df4Xz3DN5NFDgJ6vsDMkS7yct4zUppTK0nrAlKqBRjhBndpWvLQcK7ulPlyJ2vEvpp1DuWgY8ljgHLThOGe3OA==@portal2018.documents.azure.com:10255/?ssl=true}
 // COSMOSDB_DBNAME={portal2018}
 
-mongoose.connect("mongodb://portal2018:df4Xz3DN5NFDgJ6vsDMkS7yct4zUppTK0nrAlKqBRjhBndpWvLQcK7ulPlyJ2vEvpp1DuWgY8ljgHLThOGe3OA%3D%3D@portal2018.documents.azure.com:10255/?ssl=true",{native_parser:true},{useNewUrlParser: true}); //Creates a new DB, if it doesn't already exist
+mongoose.connect("mongodb://portal2018:df4Xz3DN5NFDgJ6vsDMkS7yct4zUppTK0nrAlKqBRjhBndpWvLQcK7ulPlyJ2vEvpp1DuWgY8ljgHLThOGe3OA%3D%3D@portal2018.documents.azure.com:10255/?ssl=true",{native_parser:true}); //Creates a new DB, if it doesn't already exist
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
